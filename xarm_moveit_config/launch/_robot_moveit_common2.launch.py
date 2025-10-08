0e2c65883c593dc5a22e121e295fabf902c7f24b
ansonmak <ansonmak1997@gmail.com>
2025-10-08 10:13:10 +0800

    Restored all rviz file to official package, except added one for manipulation

--- xarm_moveit_config/launch/_robot_moveit_common2.launch.py
+++ xarm_moveit_config/launch/_robot_moveit_common2.launch.py
@@ -46,8 +46,8 @@ def launch_setup(context, *args, **kwargs):
     )
 
     # rviz with moveit configuration
-    #rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_config_package_name), 'rviz', 'planner_compliance.rviz' if no_gui_ctrl.perform(context) == 'true' else 'moveit.rviz'])
-    rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_config_package_name), 'rviz', 'planner.rviz' if no_gui_ctrl.perform(context) == 'true' else 'cartesian_controller.rviz'])
+    #rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_config_package_name), 'rviz', 'planner.rviz' if no_gui_ctrl.perform(context) == 'true' else 'moveit.rviz'])
+    rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_config_package_name), 'rviz', 'door_manip.rviz'])
     rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
